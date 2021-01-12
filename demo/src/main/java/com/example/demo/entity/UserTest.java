abd1839b0c97fd1f377f2d725b1b702dff650cd1
wangdf <zerotohero1996@163.com>
2021-01-12 11:57:19 +0800

    [Java8-demo]修改用户测试实体类文件

--- demo/src/main/java/com/example/demo/entity/UserTest.java
+++ demo/src/main/java/com/example/demo/entity/UserTest.java
@@ -1,50 +1,21 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+
+/**
+ * 用户测试实体类
+ *
+ * @author Ernest
+ * @version 1.0, 10/1/2021
+ * @since 1.0.0
+ */
+@Data
 public class UserTest {
 
+    /** 用户id */
     private int id;
+    /** 用户姓名 */
     private String name;
+    /** 用户密码 */
     private String password;
-
-    public UserTest() {
-    }
-
-    public UserTest(int id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "UserTest{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
